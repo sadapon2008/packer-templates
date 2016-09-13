d0e8177290b6a51a78419ed9f1f2484f1e50294e
Tsuyoshi Sadakata <sadapon2008@gmail.com>
2016-09-13 21:29:30 +0900

    first commit

---
+++ scripts/centos6_setup2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+yum -y install gcc make kernel-devel perl
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run --nox11
+umount /mnt
+rm -f /home/vagrant/VBoxGuestAdditions.iso
+rm -f /home/vagrant/.vbox_version
+
+rm -f /etc/machine-id
+
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
+
+yum -y clean all
+
+rm -f /var/lib/dhclient/*
+rm -rf /tmp/*
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+sync
