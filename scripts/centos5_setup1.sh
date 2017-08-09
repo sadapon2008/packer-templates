63986ecbdc27e821cec52e1c8938e4b57a072a3f
Tsuyoshi Sadakata <sadapon2008@gmail.com>
2017-08-09 13:53:09 +0900

    modify centos5

--- scripts/centos5_setup1.sh
+++ scripts/centos5_setup1.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sed -i -e 's/^\(mirrorlist=http:\/\/mirrorlist\.centos\.org\)/#\1/g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i -e 's/^\(#baseurl=http:\/\/mirror\.centos\.org\/centos\/$releasever\)/baseurl=http:\/\/vault.centos.org\/5.11/g' /etc/yum.repos.d/CentOS-Base.repo
+
 sed -i "s/^#AddressFamily .*/AddressFamily inet/" /etc/ssh/sshd_config
 sed -i "s/^#UseDNS .*/UseDNS no/" /etc/ssh/sshd_config
 sed -i "s/^GSSAPIAuthentication yes/GSSAPIAuthentication no/" /etc/ssh/sshd_config
 echo 'GATEWAYDEV=eth0' >>/etc/sysconfig/network
-echo "include_only=.jp" >> /etc/yum/pluginconf.d/fastestmirror.conf
 yum -y update
 reboot
 sleep 60
